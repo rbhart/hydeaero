759330616f9c55ac1581468ae53268ea109fdf7a
rbhart <71887296+rbhart@users.noreply.github.com>
2020-11-11 11:14:00 +0000

    Update routes.py

--- routes.py
+++ routes.py
@@ -18,5 +18,33 @@ def form():
 def dashboard():
     return render_template('dashboard.html', the_title='Dashboard')
 
+@app.route('/supplier')
+def runform():
+    return render_template('supplier.html')
+
+@app.route('/output')
+def runform():
+    return render_template('output.html')
+
+@app.route('/process')
+def runform():
+    return render_template('process.html')
+
+@app.route('/input')
+def runform():
+    return render_template('input.html')
+
+@app.route('/customer')
+def runform():
+    return render_template('customer.html')
+
+@app.route('/newjob')
+def runform():
+    return render_template('newjob.html')
+
+@app.route('/complete')
+def runform():
+    return render_template('complete.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
